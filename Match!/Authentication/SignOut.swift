10ca793b601a190671d9e4375ff45f89562ce8a4
XlSolver <salvatoreflauto0@gmail.com>
2024-05-23 23:48:53 +0200

    Integrated signInWithApple + firebase console. Commented/Deleted all swiftdata code to migrate to firebase realtime database

---
+++ Match!/Authentication/SignOut.swift
@@ -0,0 +1,18 @@
+//
+//  SignOut.swift
+//  Match!
+//
+//  Created by Salvatore Flauto on 23/05/24.
+//
+
+import SwiftUI
+
+struct SignOut: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignOut()
+}
